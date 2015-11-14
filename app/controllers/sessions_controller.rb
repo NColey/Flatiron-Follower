157802d2cbce8fde2a708e7af31cca7de22b709c
NColey <nrcoley@gmail.com>
2015-11-14 00:54:54 -0500

    builds sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+
+	def new
+    
+	end
+
+	def create
+		student = Student.find_by_email(params[:email])
+		if student && student.authenticate(params[:password])
+		    log_in(student)
+		    redirect_to root_url
+		else
+		  flash.now.alert = "Sorry, that's an invalid email/password combination!"
+		  render "new"
+		end
+	end
+
+	def destroy
+		log_out
+		redirect_to root_url
+	end
+
 end

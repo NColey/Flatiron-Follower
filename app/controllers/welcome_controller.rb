110b6b2cf76a2e8843ac31db03e9554bb338d869
NColey <nrcoley@gmail.com>
2015-11-15 00:41:11 -0500

    builds successful signup form

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,10 @@
 class WelcomeController < ApplicationController
+	def index
+		@students = Student.all
+	end
+
+	def create
+		@student = Student.find(params[:student][:id])
+		redirect_to edit_student_path(@student)
+	end
 end

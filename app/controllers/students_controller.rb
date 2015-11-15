fb3d36a6b1756905a5157bb9b91d5c34a637df0b
talum <tracidini@gmail.com>
2015-11-15 14:19:02 -0500

    Cleans up scopes requested from github omniauth

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,6 +4,7 @@ class StudentsController < ApplicationController
 	end
 
   def connect
+    binding.pry
     @student = Student.find_by(id:session[:student_id])
     @student.update(auth_hash["provider"]=>auth_hash["credentials"]["token"])
     redirect_to student_profile_path(@student)

38284d0d0569b4bc43b03551279b35cd33f22866
NColey <ncoley>
2015-11-16 19:58:32 -0500

    fixes encryption key

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,12 +4,12 @@ class Student < ActiveRecord::Base
 	#validation
 	has_secure_password
 	validates_format_of :email, :with => /@flatironschool.com/
-  attr_encrypted :github, :key => :encryption_key
+  	attr_encrypted :github, :key => :encryption_key
 
 
-  def encryption_key
-    ENV['DECRYPT_GITHUB_OAUTH']
-  end
+	def encryption_key
+	  ENV['DECRYPT_GITHUB_OAUTH']
+	end
   
 
 end

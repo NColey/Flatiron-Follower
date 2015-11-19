e7fa7ede8afebd98781079a6b9591d4a3576c7c4
NColey <ncoley>
2015-11-19 11:31:52 -0500

    updates follow partial names for twitter and github

--- app/models/adapters/twitter_connection.rb
+++ app/models/adapters/twitter_connection.rb
@@ -12,13 +12,6 @@ module Adapters
       students = Student.where(cohort_id: cohort_id)
       students.each do |student|
         username = student.twitter_handle
-        # options = {
-        #   headers:{
-        #     "User-Agent" => "Flatiron Follower",
-        #     "Authorization" => "token #{auth_token}"
-        #   },
-        #   body:{}
-        # } 
         connection.put("https://api.twitter.com/1.1/friendships/create.json?screen_name=#{username}&amp;follow=true")
       end     
     end
@@ -27,13 +20,6 @@ module Adapters
      students = Student.where(cohort_id: cohort_id)
       students.each do |student|
         username = student.twitter_handle
-        # options = {
-        #   headers:{
-        #     "User-Agent" => "Flatiron Follower",
-        #     "Authorization" => "token #{auth_token}"
-        #   },
-        #   body:{}
-        # } 
         connection.delete("https://api.twitter.com/1.1/friendships/destroy.json?screen_name=#{username}")
       end     
     end

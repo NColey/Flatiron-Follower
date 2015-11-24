9b6fcb9d2b42b87afe9e21c49d6912a2b3136015
NColey <nrcoley@gmail.com>
2015-11-24 16:13:40 -0500

    updates flash messages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,7 +7,7 @@ class WelcomeController < ApplicationController
 	def create
 		@student = Student.find(params[:student][:id])
 		if @student.password_digest != nil
-			flash.now[:alert] = "Sorry this user already has an account!"
+			flash[:alert] = "Sorry this user already has an account! Please log in below or go back to sign up."
       		redirect_to login_path(@student)
       	else
 			redirect_to edit_student_path(@student)

e438c959b4afc0a2eebd4f4eec4a8dc80fa01274
NColey <nrcoley@gmail.com>
2015-12-03 22:59:50 -0500

    refactors cohorts controller to remove repetitive code

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -2,54 +2,39 @@ class CohortsController < ApplicationController
 
 
   def follow_cohort
-    
-    cohort_id = follow_params[:id]
-    @cohort = Cohort.find(cohort_id)
-    cohort_name = @cohort.name
-
-    provider = follow_params[:provider]
-    @student = Student.find_by(id: session[:student_id])
-
-    if provider == 'github'
-      token = @student.send(provider)
-      @student.github_client.follow(token, cohort_id)
-    elsif provider == 'twitter'
-      twitter = TwitterConnection.new(@student)
-      twitter.follow(cohort_id)
-    end
-    render json: {:cohort_id => cohort_id, :cohort_name => cohort_name, :provider => provider}
+    select_social_media_method("follow")
   end
 
   def unfollow_cohort
+    select_social_media_method("unfollow")
+  end
 
+  def select_social_media_method(social_media_method)
     cohort_id = follow_params[:id]
-    @cohort = Cohort.find(cohort_id)
-    cohort_name = @cohort.name
-
     provider = follow_params[:provider]
     @student = Student.find_by(id: session[:student_id])
 
     if provider == 'github'
       token = @student.send(provider)
-      @student.github_client.unfollow(token, cohort_id)
+      @student.github_client.send(social_media_method, *[token, cohort_id])
     elsif provider == 'twitter'
       twitter = TwitterConnection.new(@student)
-      twitter.unfollow(cohort_id)
+      twitter.send(social_media_method, cohort_id)
     end
-    render json: {:cohort_id => cohort_id, :cohort_name => cohort_name, :provider => provider}
-  end
 
-  #Twitter::Error::Forbidden: You've already requested to follow dodgerredhead.
-  #Twitter::Error::NotFound: No user matches for specified terms. #yifan #jessie
-  #Twitter::Error::TooManyRequests: Rate limit exceeded
-  #Twitter::Error::Forbidden: You've already requested to follow hwinn4.
-  #Twitter::Error::Forbidden: You've already requested to follow MicheleCavs.
-  #Twitter::Error::Forbidden: You can't follow yourself.
+    render_confirmation_message(provider, cohort_id)
+  end
 
   private 
 
   def follow_params
     params.permit(:id, :provider)
+  end
+
+  def render_confirmation_message(provider, cohort_id)
+    @cohort = Cohort.find(cohort_id)
+    cohort_name = @cohort.name
+    render json: {:cohort_id => cohort_id, :cohort_name => cohort_name, :provider => provider}
   end
 
 end

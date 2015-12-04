53946fe7b2c66151333ce9fdbff298e31174cd3e
NColey <nrcoley@gmail.com>
2015-12-03 23:51:08 -0500

    moves social media logic into social media model

--- app/models/social_media_manager.rb
+++ app/models/social_media_manager.rb
@@ -1,2 +1,12 @@
 class SocialMediaManager
+
+	def follow_or_unfollow_cohort(provider, cohort_id, student)
+	    if provider == 'github'
+	        token = @student.send(provider)
+	        @student.github_client.send(social_media_method, *[token, cohort_id])
+	    elsif provider == 'twitter'
+	        twitter = TwitterConnection.new(student)
+	        twitter.send(social_media_method, cohort_id)
+	    end
+  	end
 end

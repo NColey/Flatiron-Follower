5b3658060967a66a7c381fed5e35f476f5f2a6fb
talum <tracidini@gmail.com>
2015-12-12 16:42:08 -0500

    Change config mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,11 +82,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {host: host}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: 'gmail.com',
+    domain: 'flatiron-follower.herokuapp.com',
     user_name: ENV["GMAIL_ADDRESS"],
     password: ENV["GMAIL_PASSWORD"],
     authentication: :login,
